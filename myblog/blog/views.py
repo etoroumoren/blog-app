060dde06ebe265cc3fdaf0480d1f6c3ac1bfa44f
torohkiss <torohkiss@yahoo.com>
2025-08-25 12:32:40 +0100

    added views, urls and templates

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(status='publish')
+    
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts': posts
+        }
+    )
+    
+def post_detail(request, id):
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status='publish'
+    )
+    
+    return render(
+        request,
+        'blog/post/detail.html',
+        {
+            'post': post,
+        }
+    )
